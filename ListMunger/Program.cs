cba4efb81da77f688c093a6c1dc167bed4beaeb9
Iain M Norman <iain@norman.org>
2017-02-24 19:45:59 +0000

    Initial commit

---
+++ ListMunger/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ListMunger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FreqWord> FreqWords = new List<FreqWord>();
+
+            var lines = File.ReadLines(@"C:\Users\IainM\Copy\My Projects\RandomShipNames\WebUI\1_1_all_fullalpha.txt");
+            foreach (var line in lines)
+            {
+                var split = line.Split('\t');
+
+                if (split[1] == "@")
+                {
+                    FreqWords.Add(new FreqWord { Phrase = HttpUtility.HtmlDecode(split[3]).Replace("_", " "), Frequency = int.Parse(split[4]) });
+                }
+                else
+                {
+                    FreqWords.Add(new FreqWord { Phrase = HttpUtility.HtmlDecode(split[1]).Replace("_", " "), Frequency = int.Parse(split[4]) });
+                }
+            }
+
+            var mobyfile = File.OpenText(@"C:\Users\IainM\Copy\My Projects\RandomShipNames\WebUI\mobypos.txt");
+            File.Delete(@"C:\Users\IainM\Copy\My Projects\RandomShipNames\WebUI\mobyposf.txt");
+            var newfile = File.CreateText(@"C:\Users\IainM\Copy\My Projects\RandomShipNames\WebUI\mobyposf.txt");
+            string mobyLine;
+            while ((mobyLine = mobyfile.ReadLine()) != null)
+            {
+                var split = mobyLine.Split('\t');
+                var freqword = FreqWords.FirstOrDefault(x => x.Phrase == split[0]) 
+                    ?? new FreqWord { Frequency = 0};
+                newfile.WriteLine("{0}\t{1}", mobyLine, freqword.Frequency);
+                Console.WriteLine("{0}\t{1}", mobyLine, freqword.Frequency);
+            }
+
+            newfile.Close();
+            mobyfile.Close();
+            
+        }
+
+
+    }
+
+    public class FreqWord
+    {
+        public string Phrase { get; set; }
+        public int Frequency { get; set; }
+    }
+}
---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string pattern, int? limit, string alliterate)
+        {
+            if (String.IsNullOrWhiteSpace(pattern))
+                pattern = "T v A N";
+
+            if (alliterate == null)
+                alliterate = "false";
+
+            if (limit == null)
+                limit = 0;
+                
+             
+
+            var gen = Generator.Instance;
+
+            gen.GetName()
+
+            ViewBag.Name = gen.GetName(pattern, (int)limit, bool.Parse(alliterate));
+            ViewBag.Pattern = pattern;
+            ViewBag.Limit = limit;
+            ViewBag.Alliterate = alliterate;
+            return View();
+        }
+    }
+}
