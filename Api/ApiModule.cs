7b8e5d2dc90c02216d0fdceebb55735f69879898
Iain M Norman <iain@norman.org>
2017-02-25 14:46:25 +0000

    Creating nancy api

---
+++ Api/ApiModule.cs
@@ -0,0 +1,25 @@
+﻿using Api.Models;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule() : base("/api")
+        {
+            Get["/name"] = parameters => GetName();
+        }
+
+        private object GetName()
+        {
+            var gen = Generator.Instance;
+            var patterns = new List<string>() { "T v A N|p", "A N|p", "T N|p", "V T A N|p", "A V", "V A" };
+            var names = gen.GetNames(patterns, 5, false, 5);
+            return this.Response.AsJson(names);
+        }
+    }
+}
