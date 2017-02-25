4794bff1afe95e97fb19fb66c95fb726320b0ace
Iain M Norman <iain@norman.org>
2017-02-25 21:07:59 +0000

    Created Web project
    Moved db file

---
+++ Web/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = p => "Hello World";
+        }
+    }
+}
