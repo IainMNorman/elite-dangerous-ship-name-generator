342559d552b0c693411125f30ef763a42720208c
Iain M Norman <iain@norman.org>
2017-02-25 15:01:15 +0000

    Tidy

---
+++ Api/Models/Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+    public class Word
+    {
+        public Word()
+        {
+
+        }
+
+        public string Parts { get; set; }
+        public string Phrase { get; set; }
+        public int Frequency { get; set; }
+    }
+}
