7b245f2a279d280dcb994fbeef89c808e3858a9f
Iain M Norman <iain@norman.org>
2017-02-27 00:16:45 +0000

    Styling.

--- Api/ApiModule.cs
+++ Api/ApiModule.cs
@@ -9,25 +9,19 @@ namespace Api
 {
     public class ApiModule : NancyModule
     {
-        private List<string> patterns = new List<string>() { "T v A N|p", "A N|p", "T N|p", "V T A N|p", "A V", "V A" };
         private Generator gen = Generator.Instance;
 
         public ApiModule() : base("/api")
         {
-            Get["/name"] = p => GetName();
-            Get["/names"] = p => GetNames(10, 5);
-            Get["/names/{count}/{limit}"] = p => GetNames(p.count, p.limit);            
+            Get["/names"] = p => GetNames(10, 5, false, "T v A N|p,A N|p,T N|p,V T A N|p,A V,V A");
+            Get["/names/{count}/{limit}/{alliterate}/{patterns}"] = p => GetNames(p.count, p.limit, p.alliterate, p.patterns);
         }
 
-        private dynamic GetNames(int count, int limit)
+        private dynamic GetNames(int count, int limit, bool alliterate, string patterns)
         {
             if (count > 50) count = 50;
-            return Response.AsJson(gen.GetNames(patterns, limit, false, count));
-        }
-
-        private object GetName()
-        {
-            return Response.AsJson(gen.GetNames(patterns, 5, false, 1).First());
+            var patternsSplit = patterns.Split(',').ToList();
+            return Response.AsJson(gen.GetNames(patternsSplit, limit, alliterate, count));
         }
     }
 }
--- Web/WebRoot/src/app.js
+++ Web/WebRoot/src/app.js
@@ -4,23 +4,49 @@ import { inject } from 'aurelia-framework';
 @inject(HttpClient)
 export class App {
 
-  
+  shipNames = [];
+  patterns = ['T v A N|p', 'A N|p', 'T N|p', 'V T A N|p', 'A V', 'V A', 'T N [of] N'];
+  limit = 5;
+  count = 1;
+  alliterate = false;
+  showOptions = false;
 
-  constructor(http) { 
+  constructor(http) {
     http.configure(config => {
       config
         .withBaseUrl('/api/');
     });
     this.http = http;
-    this.shipNames = [];
     this.getSingleName();
   }
 
+  activate() {
+    window.addEventListener('keypress', this.handleKeyInput, false);
+  }
+
+  deactivate() {
+    window.removeEventListener('keypress', this.handleKeyInput);
+  }
+
+  handleKeyInput = (event) => {
+    if (event.code == 'KeyB' && event.ctrlKey) { this.showOptions = !this.showOptions; }
+  }
+
   getSingleName() {
-    this.http.fetch('name/?_t=' + new Date().getTime())
+
+    this.http.fetch(`names/${this.count}/${this.limit}/${this.alliterate}/${this.patterns}/?_t=${new Date().getTime()}`)
       .then(response => response.json())
       .then(data => {
-        this.shipNames.unshift(data);
+
+        if (Array.isArray(data)) {
+          data.forEach(function (element) {
+            this.shipNames.unshift(element);
+          }, this);
+        }
+        else {
+          this.shipNames.unshift(data);
+        }
       });
   }
+
 }
